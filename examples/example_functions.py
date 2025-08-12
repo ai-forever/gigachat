80e80396963ff4b993f7c93cbf8d608ab28b597d
Konstantin Krestnikov <rai220@gmail.com>
2025-08-12 12:58:14 +0300

    chore: remove old imports from examples

--- examples/example_functions.py
+++ examples/example_functions.py
@@ -2,26 +2,27 @@
 Для работы поисковой системы используется утилита из пакета gigachain.
 Установите соответствующую библиотеку с помощью команды
 
-pip install -U gigachain duckduckgo-search
+pip install -U gigachain ddgs
 
 """
 
 import json
 
-from gigachat.models import Chat, Function, FunctionParameters, Messages, MessagesRole, FunctionCall
-from gigachat import GigaChat
+from ddgs import DDGS
 
-from langchain_community.tools.ddg_search.tool import DuckDuckGoSearchRun
+from gigachat import GigaChat
+from gigachat.models import Chat, Function, FunctionParameters, Messages, MessagesRole
 
 
 def search_ddg(search_query):
     """Поиск в DuckDuckGo. Полезен, когда нужно ответить на вопросы о текущих событиях. Входными данными должен быть поисковый запрос."""
-    return DuckDuckGoSearchRun().run(search_query)
+    return DDGS().text(search_query, max_results=10)
+
 
 # Используйте токен, полученный в личном кабинете из поля Авторизационные данные
 with GigaChat(
     credentials=...,
-    model=... # Model with functions
+    model=...,  # Model with functions
 ) as giga:
     search = Function(
         name="duckduckgo_search",
@@ -62,7 +63,6 @@ with GigaChat(
             print("\033[90m" + f"  << Function result: {func_result}\n\n" + "\033[0m")
 
             messages.append(
-                Messages(role=MessagesRole.FUNCTION,
-                         content=json.dumps({"result": func_result}, ensure_ascii=False))
+                Messages(role=MessagesRole.FUNCTION, content=json.dumps({"result": func_result}, ensure_ascii=False))
             )
             function_called = True
