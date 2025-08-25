bedb42d4c1973d63d0e64af9ff9be67dee058549
Konstantin Krestnikov <rai220@gmail.com>
2025-08-25 17:04:05 +0300

    feature: Update function convert rules to support pydantic models

--- src/gigachat/models/function.py
+++ src/gigachat/models/function.py
@@ -2,7 +2,7 @@ from typing import Any, Dict, List, Optional
 
 from gigachat.models.few_shot_example import FewShotExample
 from gigachat.models.function_parameters import FunctionParameters
-from gigachat.pydantic_v1 import BaseModel
+from gigachat.pydantic_v1 import BaseModel, root_validator
 
 
 class Function(BaseModel):
@@ -17,3 +17,19 @@ class Function(BaseModel):
     few_shot_examples: Optional[List[FewShotExample]] = None
     return_parameters: Optional[Dict[Any, Any]] = None
     """Список возвращаемых параметров функции"""
+
+    @root_validator(pre=True)
+    def _fix_title_and_parameters(cls, values):
+        """Pydantic adapter (title -> name), (parameters -> properties)"""
+        if isinstance(values, dict):
+            values = dict(values)
+
+            if values.get("name") in (None, "") and values.get("title"):
+                values["name"] = values.pop("title", None)
+
+            if values.get("parameters") in (None, "", {}) and "properties" in values:
+                values["parameters"] = {
+                    "properties": values.pop("properties", {}),
+                }
+
+        return values
