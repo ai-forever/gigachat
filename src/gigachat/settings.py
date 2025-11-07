4e434b23df33bf25fcb40f2977074ae577257568
Konstantin Krestnikov <rai220@gmail.com>
2025-11-07 17:37:11 +0300

    feat: Support limit max parallel connections to API

--- src/gigachat/settings.py
+++ src/gigachat/settings.py
@@ -40,6 +40,8 @@ class Settings(BaseSettings):
     key_file: Optional[str] = None
     key_file_password: Optional[str] = None
     flags: Optional[List[str]] = None
+    max_connections: Optional[int] = None
+    """Максимальное количество одновременных соединений к API GigaChat"""
 
     class Config:
         env_prefix = ENV_PREFIX
